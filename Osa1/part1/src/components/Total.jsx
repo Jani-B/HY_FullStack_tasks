425b563e9643ef20dd251e2c9226646977927916
Jani B <aijala.j@gmail.com>
2024-08-22 15:42:20 +0300

    part1 step1 done

---
+++ Osa1/part1/src/components/Total.jsx
@@ -0,0 +1,10 @@
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.excercises1 + props.excercises2 + props.excercises3}
+    </p>
+  );
+};
+
+export default Total;
