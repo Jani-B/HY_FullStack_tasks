f9a4cb50759269bc133ff23434be83534393c33e
Jani B <aijala.j@gmail.com>
2024-09-09 19:49:26 +0300

    added useStates and statistics section

--- Osa1/unicafe/unicafe/src/App.jsx
+++ Osa1/unicafe/unicafe/src/App.jsx
@@ -3,6 +3,10 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <>
       <div>
@@ -12,6 +16,12 @@ function App() {
         <Button feedback={"good"} />
         <Button feedback={"neutral"} />
         <Button feedback={"bad"} />
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
       </div>
     </>
   );
