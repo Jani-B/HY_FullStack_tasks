6bebb8c9b4035abf5935969b3ccbbd26f3e6eca0
Jani B <aijala.j@gmail.com>
2024-09-17 16:42:11 +0300

    added reduce to calculate totals

--- Osa2/kurssitiedot_part2/src/components/Total.jsx
+++ Osa2/kurssitiedot_part2/src/components/Total.jsx
@@ -1,9 +1,15 @@
-const Total = (props) => {
-  let total = 0;
-  props.parts.map((part) => {
-    total += part.exercises;
+const Total = ({ parts }) => {
+  let exerciseArray = parts.map((part) => {
+    return part.exercises;
   });
 
+  const totalInitial = 0;
+
+  const total = exerciseArray.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, totalInitial);
+
+  console.log(total);
   return <p>Number of exercises: {total}</p>;
 };
 
